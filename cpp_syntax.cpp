8417a8a5295843a4159c58175d7a4302883e9c66
shawn-yhhuang <shawn.yhhuang@gmail.com>
2017-12-24 23:04:04 +0900

    first commit

---
+++ cpp_syntax.cpp
@@ -0,0 +1,29 @@
+using namespace
+return
+if else do while
+
+class start (int arg) {
+    public:
+        int value = 0;
+        start();
+        int Function();
+};
+
+start.value
+
+start::start()
+{
+}
+
+int start::Function(void)
+{
+    return 12;
+}
+
+
+&address
+
+*pointer
+*(pointer)
+
+
